417de2c1740df78f2dd373776296ba943040b8bc
Aulia <88412853+AuliaPutriPurwaningrum@users.noreply.github.com>
2021-08-11 11:57:21 +0700

    Add files via upload

---
+++ latihan.R
@@ -0,0 +1,45 @@
+#Dasar Pemrograman
+#R sebagai kalkulator
+2*3
+2+3
+3-2
+4/2
+sqrt(100)
+log10(1000000)
+10%%3 #modulus
+add <- 2-1
+
+#belajar tipe data
+
+
+
+
+angka <- "udinus kmmi wiht sadasa"
+class(kata)
+
+logika <- TRUE
+class(logika)
+
+#struktur data
+#skalar / atomic vector
+a <- 4
+b <- 5
+c <- 6
+var1 <- "a"
+var2 <- "b"
+var3 <- "c"
+
+add1 <- c-a
+#vector
+string1 <- c(90,80,80)
+numerik1 <- c(80,90,90)
+logika <- c(TRUE,FALSE,FALSE,FALSE)
+add1 <- (a-b)
+add2 <- (a-b)
+
+
+
+#list
+list1 <- list(nama = c("Aulia","Putri","Tasya"), umur = c("18","20","19"), status_sekolah = c(TRUE,FALSE,TRUE)
+#list
+list2 <- list
